c3ff1aa6c706a321c662eff6075a29986e7eb7b0
Rafael Greca <rafaelgreca@unifei.edu.br>
2020-10-02 18:23:23 -0300

    minor fixes + some code

--- modelo.c
+++ modelo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
 typedef struct pixel{
   //irá armazenar os valores RGB de cada pixel
@@ -8,14 +10,31 @@ typedef struct pixel{
 
 }pixel;
 
-void le_imagem_ppm(char nome_imagem[]){
+/* Variáveis globais */
+
+/** Largura da janela */
+int win_width  = 800;
+/** Altura da janela */
+int win_height = 600;
+/** Variável programa */
+int program;
+/** Vertex array */
+unsigned int VAO;
+/** Vertex buffer */
+unsigned int VBO;
+
+/*Funções*/
+
+/*Lê a imagem que é passada como parâmetro
+e armazena seus pixels em uma matriz*/
+int le_imagem(char nome_imagem[]){
 
   char id[2];
   int largura, altura, valor_maximo_pixel;
   pixel **matriz;
 
   //Abre a imagem ppm
-  FILE *imagem = fopen(nome_imagem, "r");
+  FILE *imagem = fopen(nome_imagem, "rb");
   if(!imagem){
     printf("Problema com o arquivo. \n");
   }
@@ -26,33 +45,40 @@ void le_imagem_ppm(char nome_imagem[]){
   fscanf(imagem, "%d %d\n", &largura, &altura);
   fscanf(imagem, "%d\n", &valor_maximo_pixel);
 
-  matriz = malloc (largura * sizeof(pixel));
+  if(largura > 0 && altura > 0 && valor_maximo_pixel <= 255){
 
-  if(strcmp(id, "P3") == 0){
+    matriz = malloc (largura * sizeof(pixel));
 
-    for(int i=0; i<largura; i++){
-      matriz[i] = malloc (altura * sizeof(pixel));
-    }
+    if(strcmp(id, "P3") == 0){
 
-    for(int k=0; k<altura; k++){
-      for(int l=0; l<largura; l++){
-        fscanf(imagem, "%d %d %d", &matriz[l][k].r, &matriz[l][k].g, &matriz[l][k].b);
+      for(int i=0; i<largura; i++){
+        matriz[i] = malloc (altura * sizeof(pixel));
       }
+
+      for(int k=0; k<altura; k++){
+
+        for(int l=0; l<largura; l++){
+          fscanf(imagem, "%d %d %d", &matriz[l][k].r, &matriz[l][k].g, &matriz[l][k].b);
+        }
+      
+      }
+    
     }
-  
+
+    if((strcmp(id, "P6") == 0) || (strcmp(id, "P5") == 0)){
+
+      fgetc(imagem);
+
+      while(!feof(imagem)){
+        pixel p;
+        fread(&p, sizeof(struct pixel), 1, imagem);
+        printf("%d %d %d", p.r, p.g, p.b);
+      }
+    
+    }
   }
 
-  if(strcmp(id, "P6") == 0){
-
-    while(!feof(imagem)){
-      pixel p;
-      fread(&p, sizeof(struct pixel), 1, imagem);
-      printf("%d %d %d\n", p.r, p.g, p.b);
-    }
-  
-  }
-
-  //fechar arquivos
+  //fechar arquivo da imagem
   fclose(imagem);
 }
 
@@ -67,7 +93,16 @@ int main(int argc, char *argv[]){
 
   strcpy(nome_imagem, argv[1]);
 
-  le_imagem_ppm(nome_imagem);
+  le_imagem(nome_imagem);
+
+  glutInit(&argc, argv);
+	glutInitContextVersion(3, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(win_width,win_height);
+	glutCreateWindow(argv[0]);
+	glewExperimental = GL_TRUE; 
+	glewInit();
 
   return 0;
 }
